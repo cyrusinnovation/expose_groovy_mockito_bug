5fadfa55fe524ba825b89421dbf85b7230adbd21
Matt Raibert <mattraibert@gmail.com>
2012-09-04 16:09:27 -0400

    difficult fix

---
+++ src/main/org/mockito/configuration/MockitoConfiguration.java
@@ -0,0 +1,6 @@
+package org.mockito.configuration;
+
+import com.cyrusinnovation.mockitogroovysupport.GroovyMockitoConfiguration;
+
+public class MockitoConfiguration extends GroovyMockitoConfiguration {
+}
